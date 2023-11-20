f6c820a4e182d13d983f0a86257817579870d392
DON RIKOding <127721859+Don-Riko@users.noreply.github.com>
2023-11-20 05:57:27 +0000

    Copilot Third Commit

--- comments.js
+++ comments.js
@@ -1,78 +1,16 @@
-//create a web server
-var express = require('express');
-var router = express.Router();
-var Comment = require('../models/comment');
+// Create web server
+// ------------------------------
 
-//get all comments
-router.get('/', function(req, res) {
-  Comment.find(function(err, comments) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(comments);
-    }
-  });
-});
+// Import dependencies
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
 
-//get comment by id
-router.get('/:id', function(req, res) {
-  Comment.findById(req.params.id, function(err, comment) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(comment);
-    }
-  });
-});
+// Create routes
+router.get('/comments', commentController.getComments);
+router.post('/comments', commentController.addComment);
+router.put('/comments/:id', commentController.updateComment);
+router.delete('/comments/:id', commentController.deleteComment);
 
-//add comment
-router.post('/', function(req, res) {
-  var comment = new Comment(req.body);
-  comment.save(function(err, comment) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(comment);
-    }
-  });
-});
-
-//update comment
-router.put('/:id', function(req, res) {
-  var query = {
-    _id: req.params.id
-  };
-  var update = {
-    $set: {
-      comment: req.body.comment,
-      username: req.body.username,
-      date: req.body.date
-    }
-  };
-  var options = {
-    new: true
-  };
-  Comment.findOneAndUpdate(query, update, options, function(err, comment) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(comment);
-    }
-  });
-});
-
-//delete comment
-router.delete('/:id', function(req, res) {
-  var query = {
-    _id: req.params.id
-  };
-  Comment.findOneAndRemove(query, function(err, comment) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(comment);
-    }
-  });
-});
-
+// Export routes
 module.exports = router;
