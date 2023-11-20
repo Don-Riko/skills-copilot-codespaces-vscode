72ae0383a2a1e652b613ce9a740b9fb450913b8d
DON RIKOding <127721859+Don-Riko@users.noreply.github.com>
2023-11-20 05:44:10 +0000

    Copilot Third Commit

--- comments.js
+++ comments.js
@@ -1,16 +1,78 @@
-//create a new server
-//import the http module
-const http = require('http');
+//create a web server
+var express = require('express');
+var router = express.Router();
+var Comment = require('../models/comment');
 
-// Create a server
-const server = http.createServer((req, res) => {
-  // Handle incoming requests
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world!');
+//get all comments
+router.get('/', function(req, res) {
+  Comment.find(function(err, comments) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(comments);
+    }
+  });
 });
 
-// Start the server
-server.listen(3000, 'localhost', () => {
-  console.log('Server is running on http://localhost:3000');
+//get comment by id
+router.get('/:id', function(req, res) {
+  Comment.findById(req.params.id, function(err, comment) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(comment);
+    }
+  });
 });
+
+//add comment
+router.post('/', function(req, res) {
+  var comment = new Comment(req.body);
+  comment.save(function(err, comment) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(comment);
+    }
+  });
+});
+
+//update comment
+router.put('/:id', function(req, res) {
+  var query = {
+    _id: req.params.id
+  };
+  var update = {
+    $set: {
+      comment: req.body.comment,
+      username: req.body.username,
+      date: req.body.date
+    }
+  };
+  var options = {
+    new: true
+  };
+  Comment.findOneAndUpdate(query, update, options, function(err, comment) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(comment);
+    }
+  });
+});
+
+//delete comment
+router.delete('/:id', function(req, res) {
+  var query = {
+    _id: req.params.id
+  };
+  Comment.findOneAndRemove(query, function(err, comment) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(comment);
+    }
+  });
+});
+
+module.exports = router;
